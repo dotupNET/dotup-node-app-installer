d95a5efbf932b4d442e5c559b1289c66f54604ae
Peter Ullrich <dotup.software@gmail.com>
2019-04-08 15:05:24 +0200

    Linux service EACCES

--- src/LinuxService.ts
+++ src/LinuxService.ts
@@ -3,6 +3,7 @@ import { ILinuxService } from './ILinuxService';
 import { shelly } from './Shelly';
 import { INoinConfig } from './INoinConfig';
 import fs from 'fs';
+import path from 'path';
 
 export class LinuxService {
 
@@ -11,8 +12,15 @@ export class LinuxService {
   }
 
   install(config: INoinConfig, serviceFile: string): void {
+    const srcFile = path.join(shelly.getTempDir(), `${config.service}.service`);
+    const targetFile = `/etc/systemd/system/${config.service}.service`;
+    const cmd = `sudo mv ${srcFile} ${targetFile}`;
 
-    fs.writeFileSync(`/etc/systemd/system/${config.service}.service`, serviceFile);
+    // Write to tmp
+    fs.writeFileSync(srcFile, serviceFile);
+
+    // move to target folder
+    shelly.exec(cmd);
 
     shelly.exec('sudo systemctl daemon-reload');
     shelly.exec('sudo systemctl enable motobox');
